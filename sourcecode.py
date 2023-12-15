7dd1daf81e19fd2a16bef7ea6cce31f0c7d14b36
kareawe <96830595+kareawe@users.noreply.github.com>
2023-12-15 21:49:49 +0900

    Update sourcecode.py
    update def display to 202031512 김민재

--- sourcecode.py
+++ sourcecode.py
@@ -8,17 +8,25 @@ def decode(image):
         print("DATA : ", object.data, '\n')
     return pyzbar.decode(image)
 
+def display(image, decoded_objects):
+    for decode_object in decoded_objects:
+        points = decode_object.polygon
 
+        if len(points) > 4:
+            convex_hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+            convex_hull = list(map(tuple, np.squeeze(convex_hull)))
+        else:
+            convex_hull = points
 
+        n = len(convex_hull)
 
-def display(image, decoded):
-    //draw a boundary around the decoded QR code and display the result as an image
+        for j in range(0, n):
+            cv2.line(image, convex_hull[j], convex_hull[(j + 1) % n], (255, 0, 0), 3)
+
+    cv2.imshow("Result", image)
+    cv2.waitKey(0)
 
 if __name__ == '__main__':
-    //read the image 'QR_gachon university.jpg' and use the decode function to decode the QR code
-=======
-def decode(image):
-    for object in pyzbar.decode(image):
-        print("DATA : ", object.data, '\n')
-    return pyzbar.decode(image)
->>>>>>> main
+    image = cv2. imread('QR_gachonuniversity.jpg')
+    decoded = decode(image)
+    display(image, decoded)
