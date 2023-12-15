246aaadaf7fb6fb51d153eb3ded92e303c0fa9b8
kareawe <96830595+kareawe@users.noreply.github.com>
2023-12-15 21:29:21 +0900

    Update sourcecode.py
    add 김대용code

--- sourcecode.py
+++ sourcecode.py
@@ -7,3 +7,18 @@ def decode(image):
     for object in pyzbar.decode(image):
         print("DATA : ", object.data, '\n')
     return pyzbar.decode(image)
+
+
+
+
+def display(image, decoded):
+    //draw a boundary around the decoded QR code and display the result as an image
+
+if __name__ == '__main__':
+    //read the image 'QR_gachon university.jpg' and use the decode function to decode the QR code
+=======
+def decode(image):
+    for object in pyzbar.decode(image):
+        print("DATA : ", object.data, '\n')
+    return pyzbar.decode(image)
+>>>>>>> main
