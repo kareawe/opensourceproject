b1cd57bc7de7aa6ee69e7600fd6ace7657c73475
OpenSource_Software <143888002+03JukyeongKim@users.noreply.github.com>
2023-12-15 20:05:36 +0900

    def decode 함수 부분 작성

--- sourcecode.py
+++ sourcecode.py
@@ -1,1 +1,9 @@
 #input your code
+import cv2
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+
+def decode(image):
+    for object in pyzbar.decode(image):
+        print("DATA : ", object.data, '\n')
+    return pyzbar.decode(image)
